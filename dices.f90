1e54c7ace53889e80a5d571a9fb283d0b16c0a2f
Teitei11 <stefantleal14@gmail.com>
2019-03-22 21:17:14 -0300

    Now I just have to translate to english

--- dices.f90
+++ dices.f90
@@ -1,7 +1,7 @@
 program estatistica_dos_dados
     implicit none
 
-    ! 1) Há duas funcoes em funcionamento(por enquanto só tem uma),
+    ! 1) Há duas funcoes em funcionamento,
     ! 2) Uma funcao que vai jogar  os dados até ver se morre/vive.
     ! 3) Outra que vai ficar chamando a primeira funcao para conseguir a estatistica de vidas/mortes
 
@@ -12,35 +12,36 @@ program estatistica_dos_dados
     ! Declaracao de variaveis
 
     integer :: i, j ! O que vai percorrer as listas
-    integer :: number_of_times = 10, dices
+    integer :: number_of_times, dices_result
 
     integer :: death_score = 0, life_score = 0 ! O contador de vidas
-    real(8) :: percent_life = 0, percent_death = 0
+    real(8) :: porcentagem
 
     real(8) :: data
 
-    integer :: seed = 1               ! Seed do programa
-    real*8 :: random
+    integer :: dice_number               ! Seed do programa
+    real(8) :: random
 
 
-    real*8, external    :: rkiss05  ! O que vai ser usado para gerar os numeros aleatorios
-    call kissinit(seed)
+    ! Pedindo para colocar um valor para o number_of_times
+    print *, "Put the amount of itinerations to run the code: "
+    read(*, *) number_of_times
 
     ! Percorrendo varias vezes
     do i = 0, number_of_times, 1
-      data = dices()   ! TODO: Problem while calling function
-    !  print *, data
-
+      data = dices_result()
       if ( data .eq. 1 ) then         ! Caso ele nao morreu
         life_score = life_score + 1
       else                                ! Caso ele esta vivo
         death_score = death_score + 1
-      end if
+      endif
     enddo
 
 
+    porcentagem = (dfloat(life_score) / dfloat(number_of_times)) * 100
     ! Fazendo a porcentagem
-!    print *, death_score
+    print *, "Porcentagem de sair vivo: "
+    print *, porcentagem
 
 end program estatistica_dos_dados
 
@@ -49,40 +50,34 @@ end program estatistica_dos_dados
 
      ! Esta funcao joga os dados até ver se saiu ou nao com vida
 
-integer function dices()
-      dices = 0
+integer function dices_result()
+      dices_result = 0
       life_score = 0
       death_score = 0
 	    do while (life_score  < 3 .OR. death_score < 3)
 	        ! Jogando o dado
-	      random = rkiss05()
-	      random =  1 + floor((20)*random)         ! Problema no gerador de numeros aleatorios
+        call RANDOM_NUMBER(random)    ! Problema no gerador de numeros aleatorios,
+        dice_number = 1 + FLOOR(20*random)
 
-	      ! Fazendo a contagem da resposta do dado
-		  if (random > 10) then   ! .GE. Maior ou igual
-	          death_score = death_score + 1
-		  else if (random .eq. 20) then
-	          life_score = life_score + 3
-		  else if(random .eq. 1)  then
-	          death_score = death_score + 1
-	  	  else ! random < 10
-	          life_score = life_score + 1
-		  endif
-
-        print *, "Life: ", life_score
-        print *, "Death: ", death_score
-	     ! Vendo se ja morreu ou voltou a vida
-       if(life_score > 2) then
-          dices = 1
-          return
-       endif
-
-	     if(death_score > 2) then
-	        dices = -1
-	        return
+        if (dice_number == 20) then  ! Ja alcansou o objetivo, não é mais necessário itinerar
+          dices_result = 1
+        else if (dice_number > 10) then
+          life_score = life_score + 1
+        else if (dice_number == 1) then
+            death_score = death_score + 1
+        else                             ! dice_number < 10
+          death_score = death_score + 1
         endif
 
+            ! Ouput part
+        if (life_score == 3) then
+            dices_result = 1
+        else if (death_score == 3) then
+            dices_result = -1
+        else
+          continue
+        endif
 
 	  	 enddo
 
-	END FUNCTION dices
+	END FUNCTION dices_result
